04843244418a5f211e98a5411afdd38ac6d96340
seongho9 <seongho9276@gmail.com>
2024-05-23 06:46:13 +0900

    [FIX] RedisConfig.java(redis_standalone) -> Config.java(cluster)

---
+++ monitoring-domain/src/main/java/com/fishingboat/monitoring/domain/cache/config/Config.java
@@ -0,0 +1,76 @@
+package com.fishingboat.monitoring.domain.cache.config;
+
+import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import io.lettuce.core.internal.HostAndPort;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DnsResolver;
+import io.lettuce.core.resource.MappingSocketAddressResolver;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Data
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class Config {
+
+    private final RedisConfig _RedisInfo;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(_RedisInfo.getNodes());
+        redisClusterConfiguration.setPassword(_RedisInfo.getPassword());
+        redisClusterConfiguration.setMaxRedirects(_RedisInfo.getMaxRedirects());
+
+        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enableAllAdaptiveRefreshTriggers()
+                .enablePeriodicRefresh(Duration.ofMillis(_RedisInfo.getRefreshPeriod()))
+                .build();
+        ClusterClientOptions clientOptions = ClusterClientOptions.builder()
+                .topologyRefreshOptions(clusterTopologyRefreshOptions)
+                .build();
+
+        MappingSocketAddressResolver resolver = MappingSocketAddressResolver.create(DnsResolver.unresolved(),
+                hostAndPort -> {
+                    HostAndPort andPort = HostAndPort.of(_RedisInfo.getConnectIp(), hostAndPort.getPort());
+                    return andPort;
+                });
+        ClientResources clientResources = ClientResources.builder()
+                .socketAddressResolver(resolver)
+                .build();
+
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .clientResources(clientResources)
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfiguration);
+
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
