5a9f81733a260b4b16139071c1b5c189ea9584a8
seongho9 <seongho9276@gmail.com>
2024-05-23 06:46:48 +0900

    [FEAT] properties file

---
+++ monitoring-domain/src/main/java/com/fishingboat/monitoring/domain/cache/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.fishingboat.monitoring.domain.cache.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis.cluster")
+public class RedisConfig {
+    private int maxRedirects;
+    private String password;
+    private Long refreshPeriod;
+    private String connectIp;
+    private List<String> nodes;
+
+}
